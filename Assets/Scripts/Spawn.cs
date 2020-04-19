6975ab4f147fb5d187077186109bcba1a02793cc
shariful islam <sisagor98@gmail.com>
2020-04-19 16:41:54 +0600

    first commit

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject[] balloons;
+
+
+
+    private void Start()
+    {
+        StartCoroutine(StartSpawn());
+    }
+
+    IEnumerator StartSpawn()
+    {
+        yield return new WaitForSeconds(4);
+        for(int i = 0; i < 3; i++)
+        {
+            Instantiate(balloons[i], spawnPoints[i].position, Quaternion.identity);
+        }
+        StartCoroutine(StartSpawn());
+    }
+}
